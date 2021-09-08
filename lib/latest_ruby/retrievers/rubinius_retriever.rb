c744f65f66aef9809d9f6965e46e73926a198b26
André Luis Leal Cardoso Junior <andrehjr@gmail.com>
2021-09-08 14:02:26 -0300

    Pull rbx sources from github releases API

--- lib/latest_ruby/retrievers/rubinius_retriever.rb
+++ lib/latest_ruby/retrievers/rubinius_retriever.rb
@@ -1,18 +1,11 @@
-require 'rexml/document'
+require 'json'
 
 module Latest
   class RubiniusRetriever
-
-    include REXML
-
     def retrieve(rbx)
       page = Net::HTTP.get(URI(rbx.source))
-      xml  = Document.new(page)
-      all_versions = XPath.match(xml, '//Contents//Key').map(&:text)
-      candidates = all_versions.find_all { |v| v =~ /\Arubinius-/ }
-      stables = candidates.flat_map { |v| v.scan(/-(\d\.\d\.\d)\.tar/) }.flatten
-      stables.map { |v| RubyVersion.new(v) }.max
+      latest_version = JSON.parse(page)
+      RubyVersion.new(latest_version['tag_name'].scan(/\d.\d/).first)
     end
-
   end
 end
